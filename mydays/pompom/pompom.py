b88b95bc5b46935ed48b5473175ca7038602085b
Chris Patti <feoh@feoh.org>
2019-05-30 22:18:31 -0400

    slight refactor

--- mydays/pompom/pompom.py
+++ mydays/pompom/pompom.py
@@ -12,24 +12,37 @@ def back_to_work():
     print('\a')
 
 
-def start(mini_break_time, long_break_time):
+def restore_from_saved_poms(poms_file):
     config_path = Path.home() / ".config" 
     poms_file = config_path / "Pomodoro.json"
     print(f"config_path: {config_path} poms_file: {poms_file}")
-    Pomodoros = []
+
+    try:
+        with open(poms_file,'r') as pfile:
+            print(f"Reloading saved Pomodoros from {poms_file}")
+            return(json.load(pfile))
+    except OSError:
+        print("No previously stored Pomodoros! Welcome!")
+        return {}
+
+def check_break_time(completed_poms, mini_break_time, long_break_time):
+    if (completed_poms / 4) == 0:
+        return(long_break_time, "long")
+    else:
+        return(mini_break_time, "short")
+
+
+def start(mini_break_time, long_break_time):
     # A single Pomodoro is 25 minutes long.
     pomodoro_seconds = 60 * 25
     completed_poms = 0
 
-    try:
-        with open(poms_file,'r') as pfile:
-            Pomodoros = json.load(pfile)
-    except OSError:
-        print("No previously stored Pomodoros! Welcome!")
+    Pomodoros = restore_from_saved_poms(poms_file)
 
     start_time = datetime.now()
     Pom={}
     Pom['start_time'] = str(start_time)
+    Pom['interrupts'] = 0
 
     try:
         print("Pomodoro timer starting! Hit any key when done. Hit 'Ctrl-c' to log an interrupt.")
@@ -39,13 +52,7 @@ def start(mini_break_time, long_break_time):
             completed_poms = completed_poms + 1
             print(f"{completed_poms} pomodoros complete!")
             print('\a')
-            if (completed_poms / 4) == 0:
-                print(f"Yay you earned a long break of {long_break_time} minutes!")
-                time.sleep(long_break_time)
-            else:
-                print(f"Mini break time! Get back to work in {mini_break_time} minutes!")
-                time.sleep(mini_break_time)
-
+            (length, description) = check_break_time(completed_poms, long_break_time, mini_break_time)
             back_to_work()
 
     except KeyboardInterrupt:
@@ -54,10 +61,19 @@ def start(mini_break_time, long_break_time):
         elapsed = end_time - start_time
         pom_seconds = elapsed.seconds
 
-        print("Ending session. Hit 'i' if this was an interrupt. Anything else to count this pom.")
+        print("Ending session. Hit 'i' if this was an interrupt. 'q' to end session.")
         interrupt_prompt = readchar.readchar()
-        if interrupt_prompt.lower() is not 'i':
-            completed_poms = completed_poms + 1
+
+        if interrupt_prompt == 'i':
+            Pom['interrupts'] = pom['interrupts'] + 1
+            print("Press any key when ready to resume.")
+            _=readchar.readchar()
+            continue
+        elif interrupt_prompt == 'q':
+            break
+        else:
+            raise(KeyboardInterrupt)
+
 
         print(f"Pom seconds: {pom_seconds} Finished poms: {completed_poms}")
 
