e21338f71b7e0eeadb0474e446fd516d7530432e
Chris Patti <feoh@feoh.org>
2019-06-07 02:57:47 -0400

    Incomplete initial cut of rock paper scissors

---
+++ days/13-15-text-games/rock_paper_scissors/rps.py
@@ -0,0 +1,71 @@
+from prompt_toolkit import prompt
+
+class Player():
+    def __init__(self, name):
+        self.player_name = name
+
+
+
+
+class Roll():
+    def __init__(self, name):
+        self.name = name
+        self.rolls_i_can_beat = []
+        self.rolls_that_beat_me = []
+
+
+def build_the_three_rolls():
+    rock = Roll("Rock")
+    paper = Roll("Paper")
+    scissors = Roll("Scissors")
+
+    rock.rolls_i_can_beat = [scissors]
+    rock.rolls_that_beat_me = [paper]
+    paper.rolls_i_can_beat = [rock]
+    paper.rolls_that_beat_me = [scissors]
+    scissors.rolls_i_can_beat = [paper]
+    scissors.rolls_that_beat_me = [rock]
+
+    return (rock, paper, scissors)
+
+def game_loop(player1, player2, rolls):
+    count = 1
+    while count < 3:
+        p2_roll = None # TODO: get random roll
+        p1_roll = None # TODO: have player choose a roll
+
+        outcome = p1_roll.can_defeat(p2_roll)
+
+        # display throws
+        # display winner for this round
+
+        count += 1
+
+    # Compute who won
+
+
+def print_header():
+    print("Welcome to Rock, Paper, Scissors! Python edition :)")
+
+
+def get_players_name():
+    return(prompt("Please enter your name: "))
+
+
+def main():
+    print_header()
+
+    rolls = build_the_three_rolls()
+    print(rolls)
+
+    name = get_players_name()
+
+    player1 = Player(name)
+    player2 = Player("computer")
+
+    # Doesn't work yet :)
+    # game_loop(player1, player2, rolls)
+
+
+if __name__ == "__main__":
+    main()
