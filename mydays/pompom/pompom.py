3d6ef0d6d24dbf83b763607ff65581a6d8f8e647
Chris Patti <feoh@feoh.org>
2019-07-13 02:19:20 -0400

    Re-add logbook.

--- mydays/pompom/pompom.py
+++ mydays/pompom/pompom.py
@@ -2,7 +2,9 @@ import json
 import argparse
 import fire
 import time
+import sys
 import readchar
+from logbook import RotatingFileHandler, info, notice, debug, StreamHandler
 from pathlib import Path
 from datetime import datetime
 
@@ -15,7 +17,7 @@ def back_to_work():
 def start(mini_break_time, long_break_time):
     config_path = Path.home() / ".config" 
     poms_file = config_path / "Pomodoro.json"
-    print(f"config_path: {config_path} poms_file: {poms_file}")
+    debug(f"config_path: {config_path} poms_file: {poms_file}")
     Pomodoros = []
     # A single Pomodoro is 25 minutes long.
     pomodoro_seconds = 60 * 25
@@ -25,25 +27,25 @@ def start(mini_break_time, long_break_time):
         with open(poms_file,'r') as pfile:
             Pomodoros = json.load(pfile)
     except OSError:
-        print("No previously stored Pomodoros! Welcome!")
+        info("No previously stored Pomodoros! Welcome!")
 
     start_time = datetime.now()
     Pom={}
     Pom['start_time'] = str(start_time)
 
     try:
-        print("Pomodoro timer starting! Hit any key when done. Hit 'Ctrl-c' to log an interrupt.")
+        info("Pomodoro timer starting! Hit any key when done. Hit 'Ctrl-c' to log an interrupt.")
         while True:
-            print(f"Starting next Pomodoro at: {str(datetime.now())}")
+            info(f"Starting next Pomodoro at: {str(datetime.now())}")
             time.sleep(pomodoro_seconds)
             completed_poms = completed_poms + 1
-            print(f"{completed_poms} pomodoros complete!")
+            info(f"{completed_poms} pomodoros complete!")
             print('\a')
             if (completed_poms / 4) == 0:
                 print(f"Yay you earned a long break of {long_break_time} minutes!")
                 time.sleep(long_break_time)
             else:
-                print(f"Mini break time! Get back to work in {mini_break_time} minutes!")
+                info(f"Mini break time! Get back to work in {mini_break_time} minutes!")
                 time.sleep(mini_break_time)
 
             back_to_work()
@@ -54,12 +56,12 @@ def start(mini_break_time, long_break_time):
         elapsed = end_time - start_time
         pom_seconds = elapsed.seconds
 
-        print("Ending session. Hit 'i' if this was an interrupt. Anything else to count this pom.")
+        info("Ending session. Hit 'i' if this was an interrupt. Anything else to count this pom.")
         interrupt_prompt = readchar.readchar()
         if interrupt_prompt.lower() is not 'i':
             completed_poms = completed_poms + 1
 
-        print(f"Pom seconds: {pom_seconds} Finished poms: {completed_poms}")
+        info(f"Pom seconds: {pom_seconds} Finished poms: {completed_poms}")
 
         Pom['end_time'] = str(end_time)
         Pom['elapsed'] = str(elapsed)
@@ -72,4 +74,7 @@ def start(mini_break_time, long_break_time):
         
 
 if __name__ == '__main__':
+    RotatingFileHandler("Pompom.log").push_application()
+    StreamHandler(sys.stderr, level='INFO', bubble=True).push_application()
+
     fire.Fire(start)
