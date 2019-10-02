3361eebd2ea766a0d076b58c11785a0aa7f07400
feoh <feoh@feoh.org>
2019-10-02 06:44:50 -0400

    My home inventory app re-done with SQLAlchemy! So elegant by comparison!

--- mydays/homeinventory-orm/data_models.py
+++ mydays/homeinventory-orm/data_models.py
@@ -1,10 +1,31 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 ModelBase = declarative_base()
 
 class Room(ModelBase):
     __tablename__ = "rooms"
     id = Column(Integer, primary_key=True)
-    name = Column(String)
+    name = Column(String, nullable=False)
     description = Column(String)
+    items = relationship("Item", backref="room")
+
+    def __repr__(self):
+        return f"Room Name: {self.name} Room Description: {self.description}"
+
+
+class Item(ModelBase):
+    __tablename__ = "items"
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    value = Column(Integer, nullable=False)
+    description = Column(String)
+    room_id = Column(Integer, ForeignKey('rooms.id'))
+
+    def __repr__(self):
+        return f"""
+        Item Name: {self.name}
+        Item Value: {self.value}
+        Item Description: {self.description}
+        Location: {self.room.name}"""
